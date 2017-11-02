c4f0cf0cd2803a6e895c56f5a45aec3a13f21079
Unknown <spencerbernstein09@gmail.com>
2017-11-02 16:47:55 -0400

    Beginnings of itembox

---
+++ Assets/Scripts/ItemManagerSc.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManagerSc : MonoBehaviour {
+
+    public GameObject ItemBox;
+    float boxTimer = 0;
+    bool startTimer = false;
+    public float boxRegen;
+    bool canGrabItem;
+	// Use this for initialization
+	void Start () {
+        Instantiate(ItemBox, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+        canGrabItem = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTimer)
+        {
+            boxTimer += Time.deltaTime;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide!");
+        if (canGrabItem == true)
+        {
+            Destroy(transform.GetChild(0));
+            StartCoroutine(BoxRegen());
+            //item delivery goes here
+        }
+    }
+
+    IEnumerator BoxRegen()
+    {
+        Debug.Log("BoxRegen start!");
+        canGrabItem = false;
+        startTimer = true;
+        yield return new WaitUntil(() => boxTimer >= boxRegen);
+        Instantiate(ItemBox, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+        startTimer = false;
+        boxTimer = 0;
+        canGrabItem = true;
+    }
+}
