d99779c5c0af8c9520db4f5d149f9e2922b9c077
Unknown <spencerbernstein09@gmail.com>
2017-11-09 14:48:40 -0500

    Created pipe, made pipe mat, further itemmanagerwork

--- Assets/Scripts/ItemManagerSc.cs
+++ Assets/Scripts/ItemManagerSc.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemManagerSc : MonoBehaviour {
+public class ItemManagerSc : MonoBehaviour { //handles boxes, UI, which item the player gets
 
+    int playerPos;
     public GameObject ItemBox;
     float boxTimer = 0;
     bool startTimer = false;
     public float boxRegen;
     bool canGrabItem;
+    int itemNum;
 	// Use this for initialization
 	void Start () {
         Instantiate(ItemBox, gameObject.transform.position, Quaternion.identity, gameObject.transform);
@@ -28,7 +30,7 @@ public class ItemManagerSc : MonoBehaviour {
         Debug.Log("Collide!");
         if (canGrabItem == true)
         {
-            Destroy(transform.GetChild(0));
+            Destroy(transform.GetChild(0).gameObject);
             StartCoroutine(BoxRegen());
             //item delivery goes here
         }
@@ -39,6 +41,43 @@ public class ItemManagerSc : MonoBehaviour {
         Debug.Log("BoxRegen start!");
         canGrabItem = false;
         startTimer = true;
+        itemNum = Random.Range(0, 100);
+        if (playerPos == 1)
+        {
+            if (itemNum >= 0 && itemNum < 25)
+            {
+                //A item
+            }
+            else if (itemNum >= 25 && itemNum < 40)
+            {
+                //B item
+            }
+            else if (itemNum >= 40 && itemNum < 50)
+            {
+                //C item 1
+            }
+            else if (itemNum >= 50 && itemNum < 60)
+            {
+
+            }
+            else if (itemNum >= 50 && itemNum < 60)
+            {
+
+            }
+            else if (itemNum >= 60 && itemNum < 70)
+            {
+
+            }
+            else if (itemNum >= 70 && itemNum < 80)
+            {
+
+            }
+            else if (itemNum >= 90 && itemNum <= 100)
+            {
+
+            }
+        }
+        
         yield return new WaitUntil(() => boxTimer >= boxRegen);
         Instantiate(ItemBox, gameObject.transform.position, Quaternion.identity, gameObject.transform);
         startTimer = false;
