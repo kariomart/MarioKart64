4484d5c8f0b8ba145fd86b4b91c49675266830c0
Unknown <spencerbernstein09@gmail.com>
2017-11-21 16:06:18 -0500

    Much better shell code

--- Assets/Scripts/PlayerItemSc.cs
+++ Assets/Scripts/PlayerItemSc.cs
@@ -7,16 +7,14 @@ public class PlayerItemSc : MonoBehaviour {
     PlayerMovement MovementSc;
     public GameObject Banana;
     GameObject singleBanana;
-    GameObject Banana1;
-    GameObject Banana2;
-    GameObject Banana3;
+    GameObject Trio1;
+    GameObject Trio2;
+    GameObject Trio3;
     public GameObject GreenShell;
     GameObject SingleShell;
-    GameObject GShell1;
-    GameObject GShell2;
-    GameObject GShell3;
+    GameObject BadCube;
     int TrioCount;
-    public int shellSpeed = 500;
+    public int shellSpeed = 30;
     public bool hasItem;
     public items currentItem;
     public int playerID = 0;
@@ -38,25 +36,49 @@ public class PlayerItemSc : MonoBehaviour {
             }
             else if (currentItem == items.bananaBunch && TrioCount == 0)
             {
-                Banana1 = Instantiate(Banana, transform.position - (transform.forward * .8f), Quaternion.identity, gameObject.transform);
-                Banana2 = Instantiate(Banana, transform.position - (transform.forward * 1), Quaternion.identity, gameObject.transform);
-                Banana3 = Instantiate(Banana, transform.position - (transform.forward * 1.2f), Quaternion.identity, gameObject.transform);
+                if (Trio1 != null)
+                {
+                    Trio1 = null;
+                    Trio2 = null;
+                    Trio3 = null;
+                }
+
+                Trio1 = Instantiate(Banana, transform.position - (transform.forward * .8f), Quaternion.identity, gameObject.transform);
+                Trio2 = Instantiate(Banana, transform.position - (transform.forward * 1), Quaternion.identity, gameObject.transform);
+                Trio3 = Instantiate(Banana, transform.position - (transform.forward * 1.2f), Quaternion.identity, gameObject.transform);
             }
             else if (currentItem == items.greenShell)
             {
+                SingleShell = null;
+
                 SingleShell = Instantiate(GreenShell, transform.position + (transform.forward * 1), gameObject.transform.rotation, gameObject.transform);
                 SingleShell.GetComponent<Rigidbody>().isKinematic = true;
             }
             else if (currentItem == items.greenShellTrio && TrioCount == 0)
             {
-                GShell1 = Instantiate(GreenShell, transform.position + (transform.forward * 1.5f), Quaternion.identity, gameObject.transform);
-                GShell2 = Instantiate(GreenShell, transform.position + (transform.right * 1.5f), Quaternion.identity, gameObject.transform);
-                GShell3 = Instantiate(GreenShell, transform.position + (transform.right * -1.5f), Quaternion.identity, gameObject.transform);
-                GShell1.GetComponent<ShellScript>().isTrio = true;
-                GShell2.GetComponent<ShellScript>().isTrio = true;
-                GShell3.GetComponent<ShellScript>().isTrio = true;
+                if (Trio1 != null)
+                {
+                    Trio1 = null;
+                    Trio2 = null;
+                    Trio3 = null;
+                }
+                
+                Trio1 = Instantiate(GreenShell, transform.position + (transform.forward * 1.5f), Quaternion.identity, gameObject.transform);
+                Trio2 = Instantiate(GreenShell, transform.position + (transform.right * 1.5f), Quaternion.identity, gameObject.transform);
+                Trio3 = Instantiate(GreenShell, transform.position + (transform.right * -1.5f), Quaternion.identity, gameObject.transform);
+                Trio1.GetComponent<shellScript>().isTrio = true;
+                Trio2.GetComponent<shellScript>().isTrio = true;
+                Trio3.GetComponent<shellScript>().isTrio = true;
             }
             else if (currentItem == items.redShell)
+            {
+
+            }
+            else if (currentItem == items.redShellTrio)
+            {
+
+            }
+            else if (currentItem == items.badCube)
             {
 
             }
@@ -76,17 +98,17 @@ public class PlayerItemSc : MonoBehaviour {
                 }
                 else if (TrioCount == 3)
                 {
-                    Banana3.transform.parent = null;
+                    Trio3.transform.parent = null;
                     TrioCount--;
                 }
                 else if (TrioCount == 2)
                 {
-                    Banana2.transform.parent = null;
+                    Trio2.transform.parent = null;
                     TrioCount--;
                 }
                 else if (TrioCount == 1)
                 {
-                    Banana1.transform.parent = null;
+                    Trio1.transform.parent = null;
                     TrioCount = 0;
                     hasItem = false;
                 }
@@ -96,7 +118,7 @@ public class PlayerItemSc : MonoBehaviour {
             {
                 SingleShell.transform.parent = null;
                 SingleShell.GetComponent<Rigidbody>().isKinematic = false;
-                SingleShell.GetComponent<Rigidbody>().AddForce(SingleShell.transform.forward*shellSpeed);
+                SingleShell.GetComponent<Rigidbody>().velocity = SingleShell.transform.forward*shellSpeed;
                 hasItem = false;
             }
             else if (currentItem == items.greenShellTrio)
@@ -107,29 +129,29 @@ public class PlayerItemSc : MonoBehaviour {
                 }
                 else if (TrioCount == 3)
                 {
-                    GShell3.GetComponent<ShellScript>().isTrio = false;
-                    GShell3.transform.position = transform.position + (transform.forward * 1.2f);
-                    GShell3.transform.parent = null;
-                    GShell3.GetComponent<Rigidbody>().isKinematic = false;
-                    GShell3.GetComponent<Rigidbody>().AddForce(transform.forward * shellSpeed);
+                    Trio3.GetComponent<shellScript>().isTrio = false;
+                    Trio3.transform.position = transform.position + (transform.forward * 1.2f);
+                    Trio3.transform.parent = null;
+                    Trio3.GetComponent<Rigidbody>().isKinematic = false;
+                    Trio3.GetComponent<Rigidbody>().velocity = SingleShell.transform.forward * shellSpeed;
                     TrioCount--;
                 }
                 else if (TrioCount == 2)
                 {
-                    GShell2.GetComponent<ShellScript>().isTrio = false;
-                    GShell2.transform.position = transform.position + (transform.forward * 1.2f);
-                    GShell2.transform.parent = null;
-                    GShell2.GetComponent<Rigidbody>().isKinematic = false;
-                    GShell2.GetComponent<Rigidbody>().AddForce(transform.forward * shellSpeed);
+                    Trio2.GetComponent<shellScript>().isTrio = false;
+                    Trio2.transform.position = transform.position + (transform.forward * 1.2f);
+                    Trio2.transform.parent = null;
+                    Trio2.GetComponent<Rigidbody>().isKinematic = false;
+                    Trio2.GetComponent<Rigidbody>().velocity = SingleShell.transform.forward * shellSpeed;
                     TrioCount--;
                 }
                 else if (TrioCount == 1)
                 {
-                    GShell1.GetComponent<ShellScript>().isTrio = false;
-                    GShell1.transform.position = transform.position + (transform.forward * 1.2f);
-                    GShell1.transform.parent = null;
-                    GShell1.GetComponent<Rigidbody>().isKinematic = false;
-                    GShell1.GetComponent<Rigidbody>().AddForce(transform.forward * shellSpeed);
+                    Trio1.GetComponent<shellScript>().isTrio = false;
+                    Trio1.transform.position = transform.position + (transform.forward * 1.2f);
+                    Trio1.transform.parent = null;
+                    Trio1.GetComponent<Rigidbody>().isKinematic = false;
+                    Trio1.GetComponent<Rigidbody>().velocity = SingleShell.transform.forward * shellSpeed;
                     TrioCount = 0;
                     hasItem = false;
                 }
