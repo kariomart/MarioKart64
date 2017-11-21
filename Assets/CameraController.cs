06460643f00e627ba11058e2f0758a5a0f01f839
Unknown <mnn264@nyu.edu>
2017-11-21 12:16:44 -0500

    camera tweaks

---
+++ Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float lerpSpeed;
+	Vector3 pos;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform target = player.transform;
+
+		transform.position = target.position - target.forward * 5 + Vector3.up * 1;
+//		transform.rotation = Quaternion.Slerp (transform.rotation, player.transform.rotation, Time.deltaTime);
+
+		//transform.LookAt (new Vector3(target.position.x, transform.position.y, target.position.z), Vector3.up);
+		transform.forward = Vector3.Lerp(transform.forward, new Vector3(target.forward.x, 0, target.forward.z), lerpSpeed);
+
+	}
+}
